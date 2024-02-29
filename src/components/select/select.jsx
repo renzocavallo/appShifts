dba6115b18c9c3ea0618ce8f979f99bdff0f4030
Renzo Cavallo <renzojcavallo@gmail.com>
2024-02-29 12:36:41 -0300

    feat: the select component was tuned

--- src/components/select/select.jsx
+++ src/components/select/select.jsx
@@ -1,22 +1,22 @@
-import React from "react"
+import React from "react";
 
-export const Select = ({options, hook, set}) => {
-    return(
-        <>
-            <select
-                value={hook.value}
-                onChange={(e)=>{set(e.target.value)}}
-            >
-                {options.map((option, index) => 
-                        <option 
-                            key={index}
-                            value={option.value}
-                        >   
-                            {option.text}
-                        </option>
-                    )
-                }
-            </select>
-        </>
-    )
+export const Select = ({ options, hook, set }) => {
+  const selectProps = {
+    value: hook,
+    onChange: (e) => {
+      set(e.target.value);
+    }
+  }
+
+  const optionElements = options.map((option, index) => (
+    <option key={index} value={option.value}>
+      {option.text}
+    </option>
+  ))
+
+  return (
+    <select {...selectProps}>
+      {optionElements}
+    </select>
+  )
 }
